9434b282bd7be8b174c2d5c19742d3875e24bdaf
radiffavikry <ravizh702@gmail.com>
2022-12-15 22:17:33 +0700

    add name

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://radiffavikryrizalulhaq-fe3000458.netlify.app" // TODO: replace this
+const studentName = "RADIFFA VIKRY RIZALULHAQ" // TODO: replace this
+const studentId = "FE3000458" // TODO: replace this
 
 export { url, studentName, studentId }
